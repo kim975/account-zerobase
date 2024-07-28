89035441e039cf5afbcddfd0a70b50605ba88103
kim <scilo200@gmail.com>
2024-07-29 05:29:37 +0900

    잔액 사용 API 테스트 개발

---
+++ src/test/java/com/project/account/controller/TransactionControllerTest.java
@@ -0,0 +1,63 @@
+package com.project.account.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.account.dto.TransactionDto;
+import com.project.account.dto.UseBalance;
+import com.project.account.service.TransactionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static com.project.account.type.TransactionResultType.S;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(TransactionController.class)
+class TransactionControllerTest {
+
+    @MockBean
+    private TransactionService transactionService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void successUseBalance() throws Exception {
+        //given
+        given(transactionService.useBalance(anyLong(), anyString(), anyLong()))
+                .willReturn(TransactionDto.builder()
+                        .accountNumber("1234567891")
+                        .transactedAt(LocalDateTime.now())
+                        .amount(123456L)
+                        .transactionId("transactionId")
+                        .transactionResultType(S)
+                        .build());
+        //when
+        //then
+        mockMvc.perform(post("/transaction/use")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new UseBalance.Request(1L, "1231231231", 3000L)
+                        )))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value("1234567891"))
+                .andExpect(jsonPath("$.transactionResult").value("S"))
+                .andExpect(jsonPath("$.transactionId").value("transactionId"))
+                .andExpect(jsonPath("$.amount").value(123456L));
+    }
+}
---
+++ src/test/java/com/project/account/service/TransactionServiceTest.java
@@ -0,0 +1,256 @@
+package com.project.account.service;
+
+import static com.project.account.type.AccountStatus.*;
+import static com.project.account.type.TransactionResultType.*;
+import static com.project.account.type.TransactionType.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.project.account.domain.Account;
+import com.project.account.domain.AccountUser;
+import com.project.account.domain.Transaction;
+import com.project.account.dto.AccountDto;
+import com.project.account.dto.TransactionDto;
+import com.project.account.exception.AccountException;
+import com.project.account.repository.AccountUserRepository;
+import com.project.account.repository.TransactionRepository;
+import com.project.account.type.AccountStatus;
+import com.project.account.repository.AccountRepository;
+import com.project.account.type.ErrorCode;
+import com.project.account.type.TransactionResultType;
+import com.project.account.type.TransactionType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @Mock
+    private AccountUserRepository accountUserRepository;
+
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @InjectMocks
+    private TransactionService transactionService;
+
+    @Test
+    void successUseBalance() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .build();
+
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountNumber("1000000012")
+                .balance(10000L)
+                .accountStatus(IN_USE)
+                .build();
+
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+        given(transactionRepository.save(any()))
+                .willReturn(Transaction.builder()
+                        .account(account)
+                        .transactionType(USE)
+                        .transactionResultType(S)
+                        .transactedAt(LocalDateTime.now())
+                        .amount(1000L)
+                        .balanceSnapshot(9000L)
+                        .build());
+
+        ArgumentCaptor<Transaction> captor = ArgumentCaptor.forClass(Transaction.class);
+
+        //when
+        TransactionDto transactionDto = transactionService.useBalance(10L, "1000000002", 1000L);
+
+        //then
+        verify(transactionRepository, times(1)).save(captor.capture());
+        assertEquals(1000L, captor.getValue().getAmount());
+        assertEquals(9000L, captor.getValue().getBalanceSnapshot());
+        assertEquals(S, transactionDto.getTransactionResultType());
+        assertEquals(USE, transactionDto.getTransactionType());
+        assertEquals(9000L, transactionDto.getBalanceSnapshot());
+        assertEquals(1000L, transactionDto.getAmount());
+    }
+
+    @Test
+    @DisplayName("해당 유저 없음 - 잔액 사용 실패")
+    void useBalance_UserNotFound() {
+        //given
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1231231231", 1000L));
+
+        //then
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("해당 계좌 없음 - 잔액 사용 실패")
+    void deleteAccount_AccountNotFound() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi")
+                .build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.empty());
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1231231231", 1000L));
+
+        //then
+        assertEquals(ErrorCode.ACCOUNT_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("계좌 소유주 다름 - 잔액 사용 실패")
+    void deleteAccountFailed_userUnMatch() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .build();
+        AccountUser otherUser = AccountUser.builder()
+                .id(13L)
+                .build();
+
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountUser(otherUser)
+                        .accountNumber("1000000012")
+                        .balance(0L)
+                        .build()));
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1231231231", 1000L));
+
+        //then
+        assertEquals(ErrorCode.USER_ACCOUNT_UN_MATCH, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("해지 계좌는 사용할 수 없다. - 잔액 사용 실패")
+    void deleteAccountFailed_AlreadyUnregistered() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountUser(user)
+                        .accountNumber("1000000012")
+                        .balance(0L)
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .build()));
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1231231231", 1000L));
+
+        //then
+        assertEquals(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("거래 금액이 잔액보다 큰 경우. - 잔액 사용 실패")
+    void exceedAmount_UseBalance() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .build();
+
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountNumber("1000000012")
+                .balance(100L)
+                .accountStatus(IN_USE)
+                .build();
+
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1231231231", 1000L));
+
+        //then
+        verify(transactionRepository, times(0)).save(any());
+        assertEquals(ErrorCode.AMOUNT_EXCEED_BALANCE, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("실패 트랜잭션 저장 성공")
+    void saveFailedUseTransaction() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .build();
+
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountNumber("1000000012")
+                .balance(10000L)
+                .accountStatus(IN_USE)
+                .build();
+
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+        given(transactionRepository.save(any()))
+                .willReturn(Transaction.builder()
+                        .account(account)
+                        .transactionType(USE)
+                        .transactionResultType(S)
+                        .transactedAt(LocalDateTime.now())
+                        .amount(1000L)
+                        .balanceSnapshot(9000L)
+                        .build());
+
+        ArgumentCaptor<Transaction> captor = ArgumentCaptor.forClass(Transaction.class);
+
+        //when
+        transactionService.saveFailedUseTransaction("1000000002", 1000L);
+
+        //then
+        verify(transactionRepository, times(1)).save(captor.capture());
+        assertEquals(1000L, captor.getValue().getAmount());
+        assertEquals(10000L, captor.getValue().getBalanceSnapshot());
+        assertEquals(F, captor.getValue().getTransactionResultType());
+    }
+
+}
